954640d261743fb047b246323b4d980170f46fcf
Vaal34 <5619@holbertonstudents.com>
2025-01-29 17:44:10 +0100

    Remove TasksTable component from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 
-import TasksTable from "./task/page";
-
 export default function Home() {
 
   return (
     <div className="size-full flex flex-col items-center p-10">
-       <TasksTable/>
     </div>
   );
 }
